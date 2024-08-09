dd26a8c0e445f1d30f0d7cd4d8d0f66283270e1b
OosamaAhmed <osamaahmedalkatkoti@gmail.com>
2024-08-09 19:02:58 +0300

    v.1.1 add view to show all bok and book detail

--- ebook_app/models.py
+++ ebook_app/models.py
@@ -3,13 +3,14 @@ from django.db import models
 # Create your models here.
 class EBooksModel(models.Model):
 
-	title = models.CharField(max_length = 80)
-	summary = models.TextField(max_length=2000)
-	pages = models.CharField(max_length=80)
+	title = models.CharField(max_length = 20)
+	summary = models.TextField(max_length=150)
+	pages = models.CharField(max_length=30)
 	pdf = models.FileField(upload_to='pdfs/')
 	author = models.CharField(max_length=80)
-	category = models.CharField(max_length=80)
+	category = models.CharField(max_length=50)
 	author_id = models.IntegerField(default=0)
 
 	def __str__(self):
 		return f"{self.title}"
+ 
--- ebook_app/views.py
+++ ebook_app/views.py
@@ -1,3 +1,18 @@
+ 
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import EBooksModel
 
-# Create your views here.
+
+# show all books in home page ==============================>>> 
+def home (request):
+    allbook = EBooksModel.objects.all()
+    context={'allbooks' :allbook }
+    return  render ( request ,  'home.html' ,context)
+
+
+# show one books in book page ==============================>>> 
+def book(request, pk):
+    getbook = EBooksModel.objects.get(id=pk)
+    context={'book' : getbook }
+    return  render ( request ,  'book.html' ,context)
